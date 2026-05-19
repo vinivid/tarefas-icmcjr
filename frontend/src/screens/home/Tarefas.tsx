8bc6cae5c9cc513390afe05a3253df09d616c1df
Ícaro Ramos Caye <caye2018khan@gmail.com>
2026-05-19 14:48:38 -0300

    Teste de TarefaCard
    Teste de TarefaCard agora abrange data no prazp

--- frontend/src/screens/home/Tarefas.tsx
+++ frontend/src/screens/home/Tarefas.tsx
@@ -1,11 +1,14 @@
 import { Text, View } from "react-native"
 import TarefaCard from "../../components/TarefaCard"
 
+const desc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lobortis malesuada justo, vitae ullamcorper tellus pharetra vel. In luctus, nibh in interdum placerat, orci sapien cursus lorem, sit amet tincidunt leo odio at neque. Sed pellentesque efficitur ante, ut aliquam arcu vestibulum vel. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc blandit tellus in nisi cursus, a facilisis lacus tempus. Sed rutrum a elit sit amet sagittis. Nullam malesuada commodo nunc eget fermentum.';
+const data = new Date(2026, 4, 22);
+
 export default function Tarefas() {
   return (
     <View>
       <Text style={{color: 'red'}}>Tela de tarefas</Text>
-      <TarefaCard titulo="teste" desc="testando" prazo="data" finished={false}></TarefaCard>
+      <TarefaCard titulo="teste" desc={desc} prazo={data} finished={false}></TarefaCard>
     </View>
   )
 }

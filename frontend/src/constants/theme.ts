ed93ba10b8608fa5693aa9ad6336a9dddacc1bad
Ícaro Ramos Caye <caye2018khan@gmail.com>
2026-05-26 15:29:15 -0300

    primaryMedium
    Cor primaryMedium adicionada

--- frontend/src/constants/theme.ts
+++ frontend/src/constants/theme.ts
@@ -3,6 +3,7 @@ const tintColorLight = '#0a7ea4';
 export const Colors = {
   light: {
     primary: '#6750A4',
+    primaryMedium: '#A091C6',
     primaryTint: '#D9CAFF',
     background: '#F9F7FD',
     onSurface: '#1E1E1E',

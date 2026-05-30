edd9d9e5933a730a4549e9719f33455123ca3b43
vinivid <frato.vini@gmail.com>
2026-05-30 18:27:28 -0300

    Esperar conectar

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,6 +6,7 @@ import mongoose from "mongoose";
 import { User } from "./models/usuario.model.js";
 
 dotenv.config();
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -18,15 +19,23 @@ app.get("/", (req, res) => {
 
 // Fazendo uma query simples
 app.get("/tst", async (req, res) => {
-  await mongoose.connect(process.env.MONGO_URL!);
   const usr = await User.find({ nome: "A" })
   res
     .status(200)
     .json(usr)
 })
 
-const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`Servidor na porta ${PORT}`);
-})
+async function main() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL!);
+    
+    app.listen(PORT, () => {
+      console.log(`Servidor na porta ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();

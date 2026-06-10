1f650886bfcde5dad5e2b52d1be8dbfd07616fbd
Ícaro Ramos Caye <caye2018khan@gmail.com>
2026-06-09 21:12:09 -0300

    Create tarefa.model.ts
    Modelo de tarefa no backend criado.

---
+++ backend/src/models/tarefa.model.ts
@@ -0,0 +1,88 @@
+import mongoose from "mongoose";
+
+/* Definir o modelo de tarefa */
+const tarefaSchema = new mongoose.Schema({
+    usuarioId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    titulo: {
+        type: String,
+        required: true
+    },
+
+    desc: {
+        type: String,
+        required: true
+    },
+
+    prazo: {
+        type: Date,
+        required: true
+    },
+
+    finished: {
+        type: Boolean,
+        required: true
+    }
+});
+
+export const Tarefa = mongoose.model("Tarefa", tarefaSchema);
+
+/**
+ * @brief Cria uma nova tarefa com as informações fornecidas
+ */
+export async function createTarefa(
+    usuarioId: string,
+    titulo: string,
+    desc: string,
+    prazo: Date
+) {
+    const tarefa = new Tarefa({
+        usuarioId,
+        titulo,
+        desc,
+        prazo,
+        finished: false
+    });
+
+    await tarefa.save();
+
+    return tarefa;
+}
+
+/**
+ * @brief Buscar todas as tarefas de um usuário pelo seu ID de usuário
+ * @returns Uma lista de todas as tarefas do usuário em questão (?)
+ */
+export async function findTarefasByUsuario(usuarioId: string) {
+    return await Tarefa.find({ usuarioId });
+}
+
+/**
+ * @brief Atualiza uma tarefa, encontrada pelo seu id, com novos dados
+ */
+export async function updateTarefa(
+    id: string,
+    dados: Partial<{
+        titulo: string,
+        desc: string,
+        prazo: Date,
+        finished: Boolean
+    }>
+) {
+    return await Tarefa.findByIdAndUpdate(
+        id,
+        dados,
+        { new: true }
+    );
+}
+
+/**
+ * @brief Exclui uma tarefa, encontrada por seu id
+ */
+export async function deleteTarefa(id: string) {
+    return await Tarefa.findByIdAndDelete(id);
+}
